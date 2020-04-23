9fd32fb6d76237dcaa3b7b66fa8b95879975d126
Andrea Colleoni <andrea@colleoni.info>
2020-04-23 09:51:16 +0200

    osservazioni all'esercizio

--- es-gg1.js
+++ es-gg1.js
@@ -4,9 +4,18 @@ var op1 = document.getElementById('op1');
 var op2 = document.getElementById('op2');
 var risultati = document.getElementById('risultati');
 
-document.getElementById('piu').addEventListener('click', () => {
-    var ris = (+op1.value) + (+op2.value);
-    creaRisultato(ris, '+');
+var operazioni = document.getElementsByClassName('op-btn');
+for (var i = 0; i < operazioni.length; i++) {
+    operazioni[i].addEventListener('click', (event) => {
+        var ris = eval(`${(+op1.value)} ${event.target.innerText} ${(+op2.value)}`);
+        creaRisultato(ris, event.target.innerText);
+    });
+}
+
+/*
+document.getElementById('piu').addEventListener('click', (event) => {
+    var ris = eval(`${(+op1.value)} ${event.target.innerText} ${(+op2.value)}`);
+    creaRisultato(ris, event.target.innerText);
 });
 document.getElementById('meno').addEventListener('click', () => {
     var ris = (+op1.value) - (+op2.value);
@@ -19,6 +28,17 @@ document.getElementById('per').addEventListener('click', () => {
 document.getElementById('diviso').addEventListener('click', () => {
     var ris = (+op1.value) / (+op2.value);
     creaRisultato(ris, '/');
+});
+*/
+
+document.getElementById('canc').addEventListener('click', () => {
+    /*
+    risultati.children.forEach = [].forEach;
+    risultati.children.forEach(el => risultati.removeChild(el));
+    */
+    while(risultati.children.length > 0) {
+        risultati.removeChild(risultati.children[0]);
+    }
 });
 
 function creaRisultato(ris, op) {
